81fc032e0c7bd5bcee3d02dde7751aee171c0a65
IBRAHIM AHMED AKADER <44436763+akadarAkku@users.noreply.github.com>
2020-01-13 00:01:12 +0100

    Update solution.sql

---
+++ week-1-homework/submissions/solution.sql
@@ -0,0 +1,7 @@
+show tables;
+describe tables;
+select * from (car_makers) limit 5;
+describe car_makers;
+describe countries;
+select id from countries where name = 'japan';
+select full_name from car_makers where country = 4;
